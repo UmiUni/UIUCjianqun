3bf1b15a57f5f8dbc80bc2770f6c868e29e7aa31
Chaoran Wang <superchaoran@gmail.com>
2017-05-24 05:38:30 +0000

    phone mate

--- tiantian.py
+++ tiantian.py
@@ -16,7 +16,7 @@ v0= u"您好，😊UIUC加群建群小助手😊为您服务～\n"
 v1= u"回复 1 加UIUC会计、经济、ECE大家庭;\n"
 v2= u"回复 2 加UIUC工学、商学、文理学毕业通讯录;\n"
 v3= u"回复 3 加UIUC ECE、CS找队友群;\n"
-v4= u"回复 4 加UIUC功能群:刷题面试;暑期留守儿童;食神带飞;信用卡爱好者;找朋友.\n"
+v4= u"回复 4 加UIUC功能群:刷题面试;暑期留守儿童;食神带飞;信用卡爱好者;找朋友;UIUC手机family plan.\n"
 vT =v0+v1+v2+v3+v4
 #Chaoran userid:@ef633e828340000b5518a18f66daefbf8f307a1fa96d405288a885014d8c25d5
 #汪灵欣 userid:@eb21513f32b62cd9773abc2fd5531ee05ca09af4ca926fbf896d8c89f29e46cc
@@ -101,6 +101,7 @@ def tuling_reply(msg):
         pullMembersMore(msg, u'UI食神带飞群', CurUserName)
         pullMembersMore(msg, u'天天refer', CurUserName)
         pullMembersMore(msg, u'UIUC找朋友', CurUserName)
+        pullMembersMore(msg, u'UIUC手机', CurUserName)
     else:
         itchat.send_msg(vT, CurUserName)
 
